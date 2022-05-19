1f9cf4cbd99f434e8f5ee4732971875cab6343f4
Vinnie Liu <vinnie.liu@rea-group.com>
2022-05-19 21:22:11 +1000

    Initial Commit

---
+++ SwiftUI Dynamic Scroll View/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Dynamic Scroll View
+//
+//  Created by Vinnie Liu on 19/5/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI Dynamic Scroll View/SwiftUI_Dynamic_Scroll_ViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Dynamic_Scroll_ViewApp.swift
+//  SwiftUI Dynamic Scroll View
+//
+//  Created by Vinnie Liu on 19/5/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Dynamic_Scroll_ViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
